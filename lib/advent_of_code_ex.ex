be17158688fee07f2504760ecca028582ccc3a5f
Corbin Olsen <corbin.olsen@tylertech.com>
2022-12-01 23:58:02 -0600

    Initial commit

---
+++ lib/advent_of_code_ex.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCodeEx do
+  @moduledoc """
+  Documentation for `AdventOfCodeEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCodeEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
