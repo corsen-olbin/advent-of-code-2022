8162e7184ffe5340c6f7b03eab85c1ce09676b50
corsen-olbin <corbinolsen97@gmail.com>
2022-12-16 23:51:52 -0600

    some of day 14...

--- lib/advent_of_code_ex/core/days/day11.ex
+++ lib/advent_of_code_ex/core/days/day11.ex
@@ -1,10 +1,16 @@
 defmodule AdventOfCodeEx.Core.Days.Day11 do
 
-  def part_1(_input) do
-    :unimplemented
+  def part_1(input) do
+    input
+    |> convert_input
   end
 
   def part_2(_input) do
     :unimplemented
+  end
+
+  def convert_input(input) do
+    input
+    |> String.split("\r\n")
   end
 end
--- lib/advent_of_code_ex/core/days/day14.ex
+++ lib/advent_of_code_ex/core/days/day14.ex
@@ -1,10 +1,37 @@
 defmodule AdventOfCodeEx.Core.Days.Day14 do
+  alias AdventOfCodeEx.Core.Helpers.Map2D
 
-  def part_1(_input) do
+  def part_1(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(fn str -> String.split(str, " -> ") end)
+    |> convert_inputs(%{}, 0)
+    |> IO.inspect()
+
     :unimplemented
   end
 
   def part_2(_input) do
     :unimplemented
+  end
+
+  def convert_inputs([], acc, max_y), do: { acc, max_y }
+  def convert_inputs([wall | walls], acc, max_y) do
+    { wall_map, max_y } =
+      wall
+      |> Enum.map(fn point ->
+        String.split(point, ",")
+        |> then(fn [x, y] -> {String.to_integer(x), String.to_integer(y)} end)
+      end)
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.reduce({acc, max_y}, fn [coord1, coord2], a -> add_line_to_map(a, coord1, coord2) end)
+
+    convert_inputs(walls, wall_map, max_y)
+  end
+
+  def add_line_to_map(map, {x1, y1}, {x2, y2}) do
+    for x <- x1..x2, y <- y1..y2, reduce: map do
+      {acc, max_y} -> { Map2D.put(acc, x, y, "#"), max(max_y, max(y1, y2)) }
+    end
   end
 end
